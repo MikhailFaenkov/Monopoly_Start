254b819af2ab02b1f92556272f2545cc51031c7c
Mikhail Faenkov <mihailfaenkov4386@gmail.com>
2019-04-27 16:34:39 +0300

    Began creating an easy Monopoly

--- Monopoly_Start/ViewController.swift
+++ Monopoly_Start/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cells: [UIStackView]!
+    
+    var currentCellIndex = 0 {
+        didSet {
+            currentCellIndex %= cells.count
+            updateUI()
+        }
+    }
+    
+    var currentCell: UIStackView {
+        return cells[currentCellIndex]
+    }
+    
+    var prevCell: UIStackView {
+        let index = (currentCellIndex + cells.count - 1) % cells.count
+        return cells[index]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        currentCellIndex += 1
+    }
 
+    func updateUI() {
+        updateCell(currentCell, with: "Red")
+        updateCell(prevCell, with: "")
+    }
+    
+    func updateCell(_ cell: UIStackView, with imageName: String) {
+        let horizontal = cell.arrangedSubviews.first! as! UIStackView
+        let imageView = horizontal.arrangedSubviews.first! as! UIImageView
+        imageView.image = UIImage(named: imageName)
+    }
 }
 
